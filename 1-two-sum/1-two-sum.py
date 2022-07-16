3637c268fb2e178dfc2f99dfa23616ba6bdca93c
mliriano22 <84875202+mliriano22@users.noreply.github.com>
2022-07-15 20:48:21 -0400

    Time: 636 ms (46.66%), Space: 14.2 MB (86.16%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        # this solution uses slicing instead of data mapping 
+        # data mapping like a dictionary would probably be better to avoid future errors but this was just for experimenation
+        
+        l=[]
+
+        # the for loop calculates x, the number needed to be added/ found to reach the target, for each element in nums
+        for i in range(len(nums)):
+            
+            x= target- nums[i]
+            
+            if x in nums: #this if statement counts x
+                
+                count=nums.count(x)
+                
+                if count == 1 and x*2 == target: #this if statement makes sure we do not return the same index twice
+                    continue
+                    
+                else:
+                    
+                    l.append(i) #adds to the list we created to hold the indices
+                    
+        return l #returns our list of indices
+    
