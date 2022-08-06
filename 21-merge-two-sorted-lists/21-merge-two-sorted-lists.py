e1ad2063fbfa01062fa44145c13ff8a72a9d94eb
Marilyn Liriano <84875202+mliriano22@users.noreply.github.com>
2022-08-06 13:28:13 -0400

    Time: 47 ms (21.38%), Space: 13.5 MB (59.21%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode()
+        m=dummy
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+
+                m.next=ListNode(list1.val)
+                list1=list1.next
+
+            else:
+
+                m.next=ListNode(list2.val)
+                list2=list2.next
+                
+            m=m.next
+            
+        if list1:
+            m.next=list1
+        elif list2:
+            m.next=list2
+            
+            
+        return dummy.next
+            
+              
+                
+                
+        
