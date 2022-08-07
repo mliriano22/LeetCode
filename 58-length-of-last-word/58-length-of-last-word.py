31f052b1414aba269a73ab95a810f19c824ad4f9
Marilyn Liriano <84875202+mliriano22@users.noreply.github.com>
2022-08-07 03:26:52 -0400

    Time: 29 ms (39.95%), Space: 13.7 MB (28.54%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        s=s.strip()
+        s=s.split()
+        return len(s[-1])
+            
+            
+            
