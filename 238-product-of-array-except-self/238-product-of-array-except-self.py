aea09e8c93c313d97d46e05839c12929d925b901
Marilyn Liriano <84875202+mliriano22@users.noreply.github.com>
2022-08-10 16:15:21 -0400

    Time: 326 ms (39.08%), Space: 20.9 MB (53.35%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -4,17 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-      
         answer=[]
         
         left=1 
         right=1
  
-        
         for i in range(len(nums)):
             answer.append(left) 
             left =left*nums[i] 
-            
             
         for j in range(len(nums)-1,-1,-1):
             answer[j]=answer[j]*right 
