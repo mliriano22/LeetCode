77c9126d52de397cc6c3f37da79aa4358f9b9265
Marilyn Liriano <84875202+mliriano22@users.noreply.github.com>
2022-08-10 16:12:42 -0400

    Time: 325 ms (39.45%), Space: 21 MB (47.32%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -5,20 +5,19 @@ class Solution(object):
         :rtype: List[int]
         """
       
-        answer=[]
+        answer=[] # the array we will return
         
-        left=1
+        left=1 #need to initialize with 1 for mulitplication purposes 
         right=1
-   
-        
+ 
         
         for i in range(len(nums)):
-            answer.append(left)
-            left =left*nums[i]
+            answer.append(left) # first we append as to not include the first number then we can multiply
+            left =left*nums[i] 
             
             
-        for j in range(len(nums)-1,-1,-1):
-            answer[j]=answer[j]*right
+        for j in range(len(nums)-1,-1,-1): #backwards version of our previous loop
+            answer[j]=answer[j]*right # multiplies the correct indices in our answer array 
             right= right*nums[j]
             
         return answer
