a114e79b1f1d715c0667a7834afdf3b31ae98f88
Marilyn Liriano <84875202+mliriano22@users.noreply.github.com>
2022-08-26 16:02:50 -0400

    Time: 205 ms (12.47%), Space: 13.6 MB (59.17%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -5,9 +5,16 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
+        d={}
+        count=1        
+        
+        
         for i in ransomNote:
-            if i not in magazine:
-                return False
-            if magazine.count(i) < ransomNote.count(i):
+            if i not in d:
+                d[i]= count
+            elif i in d:
+                d[i]+= 1
+            if d[i] > magazine.count(i):
                 return False
         return True
+            
